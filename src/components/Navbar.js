e77e6a55710449e14756048e685b4014aa2154b2
Madeleine Woodbury <randine25@gmail.com>
2022-01-10 08:28:27 +0100

    Offers page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
-import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
-import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
+import { useLocation, useNavigate } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -9,32 +9,32 @@ const Navbar = () => {
 
   const getStyles = (route) =>
     route === location.pathname
-      ? ['#2c2c2c', 'navbarListItemNameActive']
-      : ['#8f8f8f', 'navbarListItemName'];
+      ? ["#2c2c2c", "navbarListItemNameActive"]
+      : ["#8f8f8f", "navbarListItemName"];
 
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon fill={getStyles('/')[0]} width='36px' height='36px' />
-            <p className={getStyles('/')[1]}>Explore</p>
+          <li className='navbarListItem' onClick={() => navigate("/")}>
+            <ExploreIcon fill={getStyles("/")[0]} width='36px' height='36px' />
+            <p className={getStyles("/")[1]}>Explore</p>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/offers')}>
+          <li className='navbarListItem' onClick={() => navigate("/offers")}>
             <OfferIcon
-              fill={getStyles('/offers')[0]}
+              fill={getStyles("/offers")[0]}
               width='36px'
               height='36px'
             />
-            <p className={getStyles('/offers')[1]}>Offer</p>
+            <p className={getStyles("/offers")[1]}>Offers</p>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/profile')}>
+          <li className='navbarListItem' onClick={() => navigate("/profile")}>
             <PersonOutlineIcon
-              fill={getStyles('/profile')[0]}
+              fill={getStyles("/profile")[0]}
               width='36px'
               height='36px'
             />
-            <p className={getStyles('/profile')[1]}>Profile</p>
+            <p className={getStyles("/profile")[1]}>Profile</p>
           </li>
         </ul>
       </nav>
